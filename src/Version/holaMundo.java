79bfbb0501f87ae9ace8496d96910bfcbae35196
ZjK2 <ZjK2@ZjKPc>
2022-04-13 20:43:33 -0500

    Version1

---
+++ src/Version/holaMundo.java
@@ -0,0 +1,60 @@
+package Version;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class holaMundo extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textHola;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					holaMundo frame = new holaMundo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public holaMundo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textHola = new JTextField();
+		textHola.setBounds(79, 22, 240, 20);
+		contentPane.add(textHola);
+		textHola.setColumns(10);
+		
+		JButton btnIniciar = new JButton("Iniciar");
+		btnIniciar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textHola.setText("Hola Mundo");
+			}
+		});
+		btnIniciar.setBounds(147, 152, 89, 23);
+		contentPane.add(btnIniciar);
+	}
+}
